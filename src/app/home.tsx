89e48f4aa380cd1542c9c71de0ee2f1a3f55dee3
Maiana Soares <maiana.a.soares@gmail.com>
2025-01-09 17:34:58 -0300

    imprementacao do app nearby

---
+++ src/app/home.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import { View, Alert } from 'react-native'
+import { api } from '@/services/api'
+
+export default function Home() {
+  const [categories, setCategories] = useState()
+
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get('/categories')
+      setCategories(data)
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Categorias', 'Não foi possível carregar as categorias.')
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  return <View style={{ flex: 1 }}></View>
+}
